794a0f43fc16d8baaf9d8af748d210ffb58cf686
HHC <hnuuhc@hotmail.com>
2024-10-25 12:23:46 +0800

    改进: XmlPath的attr提取

--- src/main/java/org/haic/often/parser/xml/Element.java
+++ src/main/java/org/haic/often/parser/xml/Element.java
@@ -1,7 +1,7 @@
 package org.haic.often.parser.xml;
 
-import org.jetbrains.annotations.NotNull;
 import org.haic.often.parser.ParserStringBuilder;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 import java.util.function.Predicate;
@@ -16,171 +16,171 @@ import java.util.stream.Collectors;
  */
 public class Element extends XmlTree {
 
-	public Element(@NotNull String name) {
-		super(name);
-	}
+    public Element(@NotNull String name) {
+        super(name);
+    }
 
-	public Element(Element parent, @NotNull String name) {
-		super(parent, name);
-	}
+    public Element(Element parent, @NotNull String name) {
+        super(parent, name);
+    }
 
-	protected Element(XmlTree parent, @NotNull ParserStringBuilder node) {
-		super(parent, node);
-	}
+    protected Element(XmlTree parent, @NotNull ParserStringBuilder node) {
+        super(parent, node);
+    }
 
-	/**
-	 * 按照指定规则查询标签第一个,查询规则参照{@link #select(String)}
-	 *
-	 * @param cssQuery 查询规则
-	 * @return 查询结果
-	 */
-	public Element selectFirst(@NotNull String cssQuery) {
-		var result = select(cssQuery);
-		return result.isEmpty() ? null : result.get(0);
-	}
+    /**
+     * 按照指定规则查询标签第一个,查询规则参照{@link #select(String)}
+     *
+     * @param cssQuery 查询规则
+     * @return 查询结果
+     */
+    public Element selectFirst(@NotNull String cssQuery) {
+        var result = select(cssQuery);
+        return result.isEmpty() ? null : result.get(0);
+    }
 
-	/**
-	 * 按照指定规则查询标签,支持使用空格分割,以确保更精确的查询
-	 * <p>
-	 * 查询规则查看 {@link XmlPath#select(String)}
-	 *
-	 * @param cssQuery 查询规则
-	 * @return 查询结果
-	 */
-	@NotNull
-	public Elements select(@NotNull String cssQuery) {
-		return new XmlPath(this).select(cssQuery);
-	}
+    /**
+     * 按照指定规则查询标签,支持使用空格分割,以确保更精确的查询
+     * <p>
+     * 查询规则查看 {@link XmlPath#select(String)}
+     *
+     * @param cssQuery 查询规则
+     * @return 查询结果
+     */
+    @NotNull
+    public Elements select(@NotNull String cssQuery) {
+        return new XmlPath(this).select(cssQuery);
+    }
 
-	/**
-	 * 按照指定条件筛选元素
-	 *
-	 * @param predicate 一个 非干扰的 、无状态 的谓词，应用于每个元素以确定是否应该包含它
-	 * @return 筛选结果
-	 */
-	public Elements select(@NotNull Predicate<Element> predicate) {
-		var result = new Elements();
-		if (predicate.test(this)) return result.fluentAdd(this);
-		for (var child : childs()) if (child instanceof Element e) result.addAll(e.select(predicate));
-		return result;
-	}
+    /**
+     * 按照指定条件筛选元素
+     *
+     * @param predicate 一个 非干扰的 、无状态 的谓词，应用于每个元素以确定是否应该包含它
+     * @return 筛选结果
+     */
+    public Elements select(@NotNull Predicate<Element> predicate) {
+        var result = new Elements();
+        if (predicate.test(this)) return result.fluentAdd(this);
+        for (var child : childs()) if (child instanceof Element e) result.addAll(e.select(predicate));
+        return result;
+    }
 
-	/**
-	 * 添加属性
-	 *
-	 * @param key   属性名称
-	 * @param value 属性值
-	 * @return 当前标签
-	 */
-	public Element attr(@NotNull String key, @NotNull String value) {
-		super.attr(key, value);
-		return this;
-	}
+    /**
+     * 添加属性
+     *
+     * @param key   属性名称
+     * @param value 属性值
+     * @return 当前标签
+     */
+    public Element attr(@NotNull String key, @NotNull String value) {
+        super.attr(key, value);
+        return this;
+    }
 
-	/**
-	 * 为当前标签添加所有属性
-	 *
-	 * @param attrs 属性数组
-	 * @return 当前标签
-	 */
-	public Element addAttrs(@NotNull Map<String, String> attrs) {
-		super.addAttrs(attrs);
-		return this;
-	}
+    /**
+     * 为当前标签添加所有属性
+     *
+     * @param attrs 属性数组
+     * @return 当前标签
+     */
+    public Element addAttrs(@NotNull Map<String, String> attrs) {
+        super.addAttrs(attrs);
+        return this;
+    }
 
-	/**
-	 * 删除当前标签的指定属性
-	 *
-	 * @param key 属性名称
-	 * @return 当前标签
-	 */
-	public Element removeAttr(@NotNull String key) {
-		super.removeAttr(key);
-		return this;
-	}
+    /**
+     * 删除当前标签的指定属性
+     *
+     * @param key 属性名称
+     * @return 当前标签
+     */
+    public Element removeAttr(@NotNull String key) {
+        super.removeAttr(key);
+        return this;
+    }
 
-	/**
-	 * 设置当前节点是否为自闭合(警告: 如果设置为自闭合,在格式化时将不会输出子节点以及文本,查询不受影响)
-	 *
-	 * @param isClose true为是自闭合标签,反之同理
-	 * @return 当前节点
-	 */
-	public Element close(boolean isClose) {
-		super.close(isClose);
-		return this;
-	}
+    /**
+     * 设置当前节点是否为自闭合(警告: 如果设置为自闭合,在格式化时将不会输出子节点以及文本,查询不受影响)
+     *
+     * @param isClose true为是自闭合标签,反之同理
+     * @return 当前节点
+     */
+    public Element close(boolean isClose) {
+        super.close(isClose);
+        return this;
+    }
 
-	/**
-	 * 添加一个子节点
-	 *
-	 * @param child 子节点
-	 * @return 当前节点
-	 */
-	public Element addChild(@NotNull XmlTree child) {
-		super.addChild(child);
-		return this;
-	}
+    /**
+     * 添加一个子节点
+     *
+     * @param child 子节点
+     * @return 当前节点
+     */
+    public Element addChild(@NotNull XmlTree child) {
+        super.addChild(child);
+        return this;
+    }
 
-	/**
-	 * 添加一个子节点
-	 *
-	 * @param child 子节点
-	 * @return 当前节点
-	 */
-	public Element addChild(@NotNull String child) {
-		super.addChild(child);
-		return this;
-	}
+    /**
+     * 添加一个子节点
+     *
+     * @param child 子节点
+     * @return 当前节点
+     */
+    public Element addChild(@NotNull String child) {
+        super.addChild(child);
+        return this;
+    }
 
-	/**
-	 * 添加多个子节点
-	 *
-	 * @param childs 子节点数组
-	 * @return 当前节点
-	 */
-	public Element addChilds(@NotNull XmlChilds childs) {
-		super.addChilds(childs);
-		return this;
-	}
+    /**
+     * 添加多个子节点
+     *
+     * @param childs 子节点数组
+     * @return 当前节点
+     */
+    public Element addChilds(@NotNull XmlChilds childs) {
+        super.addChilds(childs);
+        return this;
+    }
 
-	/**
-	 * 删除指定索引的子节点
-	 *
-	 * @param i 指定索引参数
-	 * @return 当前节点
-	 */
-	public Element removeChild(int i) {
-		super.removeChild(i);
-		return this;
-	}
+    /**
+     * 删除指定索引的子节点
+     *
+     * @param i 指定索引参数
+     * @return 当前节点
+     */
+    public Element removeChild(int i) {
+        super.removeChild(i);
+        return this;
+    }
 
-	/**
-	 * 删除满足条件的子节点
-	 *
-	 * @param filter 条件函数
-	 * @return 当前节点
-	 */
-	public Element removeIf(Predicate<Object> filter) {
-		super.removeIf(filter);
-		return this;
-	}
+    /**
+     * 删除满足条件的子节点
+     *
+     * @param filter 条件函数
+     * @return 当前节点
+     */
+    public Element removeIf(Predicate<Object> filter) {
+        super.removeIf(filter);
+        return this;
+    }
 
-	/**
-	 * 获取当前节点的父节点
-	 *
-	 * @return 当前节点的父节点
-	 */
-	public Element parent() {
-		return (Element) super.parent();
-	}
+    /**
+     * 获取当前节点的父节点
+     *
+     * @return 当前节点的父节点
+     */
+    public Element parent() {
+        return (Element) super.parent();
+    }
 
-	/**
-	 * 获取当前节点的所有子元素
-	 *
-	 * @return 所有子元素
-	 */
-	public Elements childElements() {
-		return super.childs().stream().filter(e -> e instanceof XmlTree).map(e -> (Element) e).collect(Collectors.toCollection(Elements::new));
-	}
+    /**
+     * 获取当前节点的所有子元素
+     *
+     * @return 所有子元素
+     */
+    public Elements childElements() {
+        return super.childs().stream().filter(XmlTree.class::isInstance).map(Element.class::cast).collect(Collectors.toCollection(Elements::new));
+    }
 
 }
--- src/main/java/org/haic/often/parser/xml/XmlPath.java
+++ src/main/java/org/haic/often/parser/xml/XmlPath.java
@@ -1,7 +1,7 @@
 package org.haic.often.parser.xml;
 
-import org.jetbrains.annotations.NotNull;
 import org.haic.often.parser.ParserStringBuilder;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.stream.Collectors;
 
@@ -14,129 +14,129 @@ import java.util.stream.Collectors;
  */
 public class XmlPath {
 
-	private final Elements es;
+    private final Elements es;
 
-	public XmlPath(@NotNull String body) {
-		this(Document.parse(body));
-	}
+    public XmlPath(@NotNull String body) {
+        this(Document.parse(body));
+    }
 
-	public XmlPath(Element e) {
-		this(new Elements().fluentAdd(e));
-	}
+    public XmlPath(Element e) {
+        this(new Elements().fluentAdd(e));
+    }
 
-	public XmlPath(Elements es) {
-		this.es = es;
-	}
+    public XmlPath(Elements es) {
+        this.es = es;
+    }
 
-	/**
-	 * 按照指定规则查询标签,支持使用空格分割,以确保更精确的查询
-	 * <p>
-	 * 例:
-	 * <blockquote>
-	 * <pre> 	@ - @head - 查询当前节点名称为head的子节点</pre>
-	 * <pre> 	# - #stop - 查询属性名id值为stop的标签节点</pre>
-	 * <pre> 	. - .stop - 查询属性名class值为stop的标签节点</pre>
-	 * <pre>	! - !name - 查询标签名不匹配相等的标签节点</pre>
-	 * <pre>	name - 查询标签名匹配相等的标签节点</pre>
-	 * <pre>	name[key] - 查询标签名并且属性名都匹配相等的标签节点</pre>
-	 * <pre>	name[key=value] - 查询标签名并且属性名和值都匹配相等的标签节点</pre>
-	 * <pre>	name[key!=value] - 查询标签名并且属性名和值都匹配且不包含匹配属性值的标签节点</pre>
-	 * <pre>	name[!key] - 查询标签名匹配且不包含匹配属性名标签节点</pre>
-	 * <pre>	name[!class|!src] - 管道符'|'连接多个属性筛选</pre>
-	 * </blockquote>
-	 *
-	 * @param cssQuery 查询规则
-	 * @return 查询结果
-	 */
-	@NotNull
-	public Elements select(String cssQuery) {
-		var es = new Elements(this.es);
-		for (var sb = new ParserStringBuilder(cssQuery); sb.stripLeading().site() < sb.length(); sb.offset(1)) {
-			switch (sb.charAt()) {
-				case '.' -> {
-					var value = sb.interceptOrEof(' ');
-					es = es.select(e -> e.containsAttr("class") && value.equals(e.attr("class")));
-				}
-				case '#' -> {
-					var value = sb.interceptOrEof(' ');
-					es = es.select(e -> e.containsAttr("id") && value.equals(e.attr("id")));
-				}
-				case '@' -> {
-					var value = sb.interceptOrEof(' ');
-					if (es.size() != 1) throw new IllegalStateException("参数 " + value + " 错误,查询对象不为Element类型");
-					es = es.get(0).childElements().stream().filter(l -> l.name().equals(value)).collect(Collectors.toCollection(Elements::new));
-				}
-				default -> {
-					var css = new StringBuilder();
-					var attrs = "";
-					while (sb.isNoOutBounds()) {
-						var c = sb.charAt();
-						if (c == '[') {
-							attrs = sb.intercept(']');
-							break;
-						} else if (c == ' ') {
-							break;
-						}
-						css.append(c);
-						sb.offset(1);
-					}
-					if (css.charAt(0) == '!') {
-						var name = css.substring(1);
-						if (attrs.isEmpty()) {
-							es = es.select(e -> !e.name().equals(name));
-						} else {
-							for (var attr : attrs.split("\\|")) {
-								int indexAttr = attr.indexOf("=");
-								if (indexAttr == -1) { // 不存在等号
-									if (attr.startsWith("!")) {
-										var thisAttr = attr.substring(1);
-										es = thisAttr.isEmpty() ? es.select(e -> !e.name().equals(name) && e.attrIsEmpty()) : es.select(e -> !e.name().equals(name) && !e.containsAttr(thisAttr));
-									} else {
-										es = es.select(e -> !e.name().equals(name) && e.containsAttr(attr));
-									}
-								} else {
-									var key = attr.substring(0, indexAttr);
-									var value = attr.charAt(attr.length() - 1) == '\'' ? attr.substring(indexAttr + 2, attr.length() - 1) : attr.substring(indexAttr + 1);
-									if (key.endsWith("!")) {
-										var thisKey = key.substring(0, key.length() - 1);
-										es = es.select(e -> !e.name().equals(name) && e.containsAttr(thisKey) && !value.equals(e.attr(thisKey)));
-									} else {
-										es = es.select(e -> !e.name().equals(name) && e.containsAttr(key) && value.equals(e.attr(key)));
-									}
-								}
-							}
-						}
-					} else {
-						var name = css.toString();
-						if (attrs.isEmpty()) {
-							es = es.select(e -> e.name().equals(name));
-						} else {
-							for (var attr : attrs.split("\\|")) {
-								int indexAttr = attr.indexOf("=");
-								if (indexAttr == -1) { // 不存在等号
-									if (attr.startsWith("!")) {
-										var thisAttr = attr.substring(1);
-										es = thisAttr.isEmpty() ? es.select(e -> e.name().equals(name) && e.attrIsEmpty()) : es.select(e -> e.name().equals(name) && !e.containsAttr(thisAttr));
-									} else {
-										es = es.select(e -> e.name().equals(name) && e.containsAttr(attr));
-									}
-								} else {
-									var key = attr.substring(0, indexAttr);
-									var value = attr.charAt(attr.length() - 1) == '\'' ? attr.substring(indexAttr + 2, attr.length() - 1) : attr.substring(indexAttr + 1);
-									if (key.endsWith("!")) {
-										var thisKey = key.substring(0, key.length() - 1);
-										es = es.select(e -> e.name().equals(name) && e.containsAttr(thisKey) && !value.equals(e.attr(thisKey)));
-									} else {
-										es = es.select(e -> e.name().equals(name) && e.containsAttr(key) && value.equals(e.attr(key)));
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		return es;
-	}
+    /**
+     * 按照指定规则查询标签,支持使用空格分割,以确保更精确的查询
+     * <p>
+     * 例:
+     * <blockquote>
+     * <pre> 	@ - @head - 查询当前节点名称为head的子节点</pre>
+     * <pre> 	# - #stop - 查询属性名id值为stop的标签节点</pre>
+     * <pre> 	. - .stop - 查询属性名class值为stop的标签节点</pre>
+     * <pre>	! - !name - 查询标签名不匹配相等的标签节点</pre>
+     * <pre>	name - 查询标签名匹配相等的标签节点</pre>
+     * <pre>	name[key] - 查询标签名并且属性名都匹配相等的标签节点</pre>
+     * <pre>	name[key=value] - 查询标签名并且属性名和值都匹配相等的标签节点</pre>
+     * <pre>	name[key!=value] - 查询标签名并且属性名和值都匹配且不包含匹配属性值的标签节点</pre>
+     * <pre>	name[!key] - 查询标签名匹配且不包含匹配属性名标签节点</pre>
+     * <pre>	name[!class|!src] - 管道符'|'连接多个属性筛选</pre>
+     * </blockquote>
+     *
+     * @param cssQuery 查询规则
+     * @return 查询结果
+     */
+    @NotNull
+    public Elements select(String cssQuery) {
+        var es = new Elements(this.es);
+        for (var sb = new ParserStringBuilder(cssQuery); sb.stripLeading().site() < sb.length(); sb.offset(1)) {
+            switch (sb.charAt()) {
+                case '.' -> {
+                    var value = sb.interceptOrEof(' ');
+                    es = es.select(e -> e.containsAttr("class") && value.equals(e.attr("class")));
+                }
+                case '#' -> {
+                    var value = sb.interceptOrEof(' ');
+                    es = es.select(e -> e.containsAttr("id") && value.equals(e.attr("id")));
+                }
+                case '@' -> {
+                    var value = sb.interceptOrEof(' ');
+                    if (es.size() != 1) throw new IllegalStateException("参数 " + value + " 错误,查询对象不为Element类型");
+                    es = es.get(0).childElements().stream().filter(l -> l.name().equals(value)).collect(Collectors.toCollection(Elements::new));
+                }
+                default -> {
+                    var css = new StringBuilder();
+                    var attrs = "";
+                    while (sb.isNoOutBounds()) {
+                        var c = sb.charAt();
+                        if (c == '[') {
+                            attrs = sb.intercept(']');
+                            break;
+                        } else if (c == ' ') {
+                            break;
+                        }
+                        css.append(c);
+                        sb.offset(1);
+                    }
+                    if (css.charAt(0) == '!') {
+                        var name = css.substring(1);
+                        if (attrs.isEmpty()) {
+                            es = es.select(e -> !e.name().equals(name));
+                        } else {
+                            for (var attr : attrs.split("\\|")) {
+                                int indexAttr = attr.indexOf("=");
+                                if (indexAttr == -1) { // 不存在等号
+                                    if (attr.startsWith("!")) {
+                                        var thisAttr = attr.substring(1);
+                                        es = thisAttr.isEmpty() ? es.select(e -> !e.name().equals(name) && e.attrIsEmpty()) : es.select(e -> !e.name().equals(name) && !e.containsAttr(thisAttr));
+                                    } else {
+                                        es = es.select(e -> !e.name().equals(name) && e.containsAttr(attr));
+                                    }
+                                } else {
+                                    var key = attr.substring(0, indexAttr);
+                                    var value = attr.charAt(attr.length() - 1) == '\'' || attr.charAt(attr.length() - 1) == '"' ? attr.substring(indexAttr + 2, attr.length() - 1) : attr.substring(indexAttr + 1);
+                                    if (key.endsWith("!")) {
+                                        var thisKey = key.substring(0, key.length() - 1);
+                                        es = es.select(e -> !e.name().equals(name) && e.containsAttr(thisKey) && !value.equals(e.attr(thisKey)));
+                                    } else {
+                                        es = es.select(e -> !e.name().equals(name) && e.containsAttr(key) && value.equals(e.attr(key)));
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        var name = css.toString();
+                        if (attrs.isEmpty()) {
+                            es = es.select(e -> e.name().equals(name));
+                        } else {
+                            for (var attr : attrs.split("\\|")) {
+                                int indexAttr = attr.indexOf("=");
+                                if (indexAttr == -1) { // 不存在等号
+                                    if (attr.startsWith("!")) {
+                                        var thisAttr = attr.substring(1);
+                                        es = thisAttr.isEmpty() ? es.select(e -> e.name().equals(name) && e.attrIsEmpty()) : es.select(e -> e.name().equals(name) && !e.containsAttr(thisAttr));
+                                    } else {
+                                        es = es.select(e -> e.name().equals(name) && e.containsAttr(attr));
+                                    }
+                                } else {
+                                    var key = attr.substring(0, indexAttr);
+                                    var value = attr.charAt(attr.length() - 1) == '\'' ? attr.substring(indexAttr + 2, attr.length() - 1) : attr.substring(indexAttr + 1);
+                                    if (key.endsWith("!")) {
+                                        var thisKey = key.substring(0, key.length() - 1);
+                                        es = es.select(e -> e.name().equals(name) && e.containsAttr(thisKey) && !value.equals(e.attr(thisKey)));
+                                    } else {
+                                        es = es.select(e -> e.name().equals(name) && e.containsAttr(key) && value.equals(e.attr(key)));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return es;
+    }
 
 }
